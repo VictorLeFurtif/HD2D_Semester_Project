6f7de0da1cdee59f23014e9d8eecc9ba79cad604
unknown <v.baz@RUBIKA.CA.LOCAL>
2026-02-17 13:32:37 -0500

    Corentin need to done 8 direction

--- Project_HD2D_Unity/Assets/Scripts/BillBoard/SpriteBillboard.cs
+++ Project_HD2D_Unity/Assets/Scripts/BillBoard/SpriteBillboard.cs
@@ -1,29 +1,61 @@
 using System;
+using Enum;
 using UnityEngine;
 
 public class SpriteBillboard : MonoBehaviour
 {
-    #region Link
-
-        public Camera cam;
-
-    #endregion
-
     #region Variables
 
-        [SerializeField] private bool freezeXZAxis = true;
+    [SerializeField] private BillboardType billboardType;
+    [SerializeField] private Transform cameraTransform;
+    
+    [Header("Lock Rotation")]
+    [SerializeField] private bool lockX;
+    [SerializeField] private bool lockY;
+    [SerializeField] private bool lockZ;
+
+    
+    private Vector3 originalRotation;
 
     #endregion
 
+    #region Unity Lifecycle
+
+    private void Awake()
+    {
+        originalRotation = cameraTransform.rotation.eulerAngles;
+    }
+    
     private void Update()
     {
-        if (freezeXZAxis)
-        {
-            transform.rotation = Quaternion.Euler(0f, cam.transform.rotation.eulerAngles.y, 0f);
-        }
-        else
-        {
-            transform.rotation = cam.transform.rotation;
-        }
+        DisplaySpriteBillboard();
     }
+
+    #endregion
+
+    #region Methods
+
+    private void DisplaySpriteBillboard()
+    {
+        switch (billboardType)
+        {
+            case BillboardType.LookAtCamera:
+                transform.LookAt(cameraTransform.position, Vector3.up);
+                break;
+            case BillboardType.CameraForward:
+                transform.forward = cameraTransform.forward;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        
+        Vector3 rotation = transform.rotation.eulerAngles;
+        
+        if (lockX) { rotation.x = originalRotation.x; }
+        if (lockY) { rotation.y = originalRotation.y; }
+        if (lockZ) { rotation.z = originalRotation.z; }
+    }
+
+    #endregion
+    
 }
