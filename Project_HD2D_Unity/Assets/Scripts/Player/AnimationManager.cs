32e50c85abcc563346a6ef5f3b95473d374c051e
CorentinH <hautcoeur.corentin@gmail.com>
2026-02-17 15:22:49 -0500

    8 sided animation + cool blend tree

--- Project_HD2D_Unity/Assets/Scripts/Player/AnimationManager.cs
+++ Project_HD2D_Unity/Assets/Scripts/Player/AnimationManager.cs
@@ -2,19 +2,37 @@ using UnityEngine;
 
 public class AnimationManager : MonoBehaviour
 {
-    #region Variables
-
-    private static readonly int Velocity = Animator.StringToHash("Velocity");
-    
+    #region Link
+    public Camera cam;
+    [SerializeField] private Transform mainTransform;
     [SerializeField] private Animator animator;
-
+    [SerializeField] private SpriteRenderer spriteRenderer;
     #endregion
 
     #region Public Methods
 
     public void HandleAnimation(float velocity)
     {
-        animator.SetFloat(Velocity, velocity);
+        animator.SetFloat("Velocity", velocity);
+    }
+    
+    private void LateUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 camForward = Vector3.ProjectOnPlane(cam.transform.forward, Vector3.up).normalized;
+        Vector3 camRight = Vector3.ProjectOnPlane(cam.transform.right, Vector3.up).normalized;
+        
+        Vector3 moveDirection = (camForward * v + camRight * h).normalized;
+
+        if (moveDirection != Vector3.zero)
+        {
+            mainTransform.forward = moveDirection;
+
+            animator.SetFloat("moveX", h);
+            animator.SetFloat("moveY", v);
+        }
     }
 
     #endregion
