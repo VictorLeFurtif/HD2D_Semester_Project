ec659aa73f09c8de7501e893a45dc193d5acccfd
Victor Baz <>
2026-02-26 17:18:16 -0500

    push before statemachine player

--- Project_HD2D_Unity/Assets/Scripts/Ui/UiManager.cs
+++ Project_HD2D_Unity/Assets/Scripts/Ui/UiManager.cs
@@ -1,13 +1,36 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 
 public class UiManager : MonoBehaviour
 {
+    #region Variables
+    
     [SerializeField] private Slider lifeBarSlider;
+    [SerializeField] private TMP_Text energyText;
 
+    #endregion
+
+    #region Helper
+    
+    private void UpdateText(TMP_Text tmpText, string text)
+    {
+        tmpText.text = text;
+    }
+
+    #endregion
+
+    #region Utility
+    
     private void UpdateLifeBar(float newValue)
     {
         this.UpdateSlider(lifeBarSlider, newValue);
     }
+
+    private void UpdateEnergyTxt(float newValue) =>
+        UpdateText(energyText, newValue.ToString());
+
+    #endregion
+    
 }
