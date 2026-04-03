0920a7bb46b22d841756bd703ded7f61e95a13c6
Shulkrex <alexthebasque@gmail.com>
2026-04-03 16:01:06 -0400

    Test snapping

--- Project_HD2D_Unity/Assets/Scripts/Entity/Player/State/PlayerAirState/PlayerInAirBase.cs
+++ Project_HD2D_Unity/Assets/Scripts/Entity/Player/State/PlayerAirState/PlayerInAirBase.cs
@@ -9,6 +9,20 @@ public abstract class PlayerInAirBase : PlayerBaseState
     {   
         HandleMovement(psc);
         HandleAnimation(psc);
+
+        CapsuleCollider collider = psc.StateMachine.GetComponentInChildren<CapsuleCollider>();
+        Vector3 snapOrigin = psc.Controller.transform.position +
+                             psc.Controller.transform.forward * collider.radius +
+                             Vector3.down * (collider.height / 2f - collider.radius);
+        Ray platformSnapRay = new Ray(snapOrigin, Vector3.down);
+        Debug.DrawRay(platformSnapRay.origin, platformSnapRay.direction * collider.radius, Color.cyan);
+        if (Physics.Raycast(platformSnapRay, out RaycastHit hit, collider.radius))
+        {
+            if (hit.normal == Vector3.up)
+            {
+                psc.StateMachine.transform.position = hit.point + Vector3.up * collider.height / 2f;
+            }
+        }
     }
 
     protected void AirControl(PlayerStateContext psc)
